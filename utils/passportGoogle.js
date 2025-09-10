734e9dd44feec2894c2979c091be7326137556e1
the white rabbit <khaledsaoud237@gmail.com>
2025-09-10 12:09:51 +0100

    creed-backend

--- utils/passportGoogle.js
+++ utils/passportGoogle.js
@@ -10,16 +10,12 @@ const callback =
 console.log(callback);
 
 module.exports = (passport) => {
-  console.log(
-    "Google OAuth callback URL in use:",
-    process.env.GOOGLE_CALLBACK_URL_PRD
-  );
   passport.use(
     new GoogleStrategy(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: callback,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL_PRD,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
